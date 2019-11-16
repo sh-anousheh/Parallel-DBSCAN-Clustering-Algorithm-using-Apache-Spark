68ada6f8155eb16c3e24a85e3f94509145d1af5f
sh-anousheh <sh-anousheh@users.noreply.github.com>
2019-11-16 10:54:17 -0500

    - Read and Pring the data

--- src/main/java/Parallel/DBSCAN/App.java
+++ src/main/java/Parallel/DBSCAN/App.java
@@ -1,5 +1,6 @@
 package Parallel.DBSCAN;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,10 +14,24 @@ public class App {
 		SparkConf conf = new SparkConf().setAppName("App2").setMaster("local");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
-		List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
-		JavaRDD<Integer> distData = sc.parallelize(data);
-
+		// List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+		// JavaRDD<Integer> distData = sc.parallelize(data);
 		// System.out.println(distData.reduce((a, b) -> a + b));
+		// ----------------------------------------------------
+
+		String fileName = "stp.txt";
+
+		File currentDirFile = new File("");
+
+		String root = currentDirFile.getAbsolutePath();
+
+		String inputFile = root + "\\" + fileName;
+
+		JavaRDD<String> distFile = sc.textFile(inputFile);
+
+		for (String S : distFile.collect()) {
+			System.out.println(S);
+		}
 
 	}
 
